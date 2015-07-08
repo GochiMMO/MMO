3157895be7331525875f3428b0819700c467e8b5
Junichi Nonaka <stardust.0510.milkyway@i.softbank.jp>
2015-07-08 17:27:29 +0900

    サーバーを立てるスクリプト、MatchingServer.csを作成、中身を仮確定

--- mmo/Assets/Script/MatchingServer.cs
+++ mmo/Assets/Script/MatchingServer.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MatchingServer : MonoBehaviour {
+public class MatchingServer : Photon.MonoBehaviour {
+    [SerializeField]
+    string roomName;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private PhotonView myPhotonView;
+
+    // Use this for initialization
+    void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1");  //接続設定
+    }
+
+    //ロビーに接続したかどうか
+    public void OnJoinedLobby()
+    {
+        Debug.Log("JoinLobby");
+        PhotonNetwork.CreateRoom(roomName); //roomNameという名前の部屋を立てる
+        //PhotonNetwork.JoinRandomRoom();
+    }
+
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+
+    }
 }

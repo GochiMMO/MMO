b8246d9cb953e70981f63a7fb7a62bdff9e5d2c3
MarkⅡ <a13014@sangi.jp>
2015-11-24 12:29:38 +0900

    スキルリスト　４職分　フレーム以外の画像差し替え終了。 現在スキルポイントを見れるようにした

---
+++ mmo/Assets/Script/GUI/Skill/NowPoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NowPoint : MonoBehaviour {
+
+    public static PlayerData playerData;  // プレイヤーデータ 
+    private int now_skillpoint ;    // 現在所持スキルポイント
+	
+    void Start () {
+        // 初期化
+        this.now_skillpoint = playerData.skillPoint;
+	}
+	
+	void Update () {
+        //　テキストの表示
+        this.GetComponent<Text>().text = now_skillpoint.ToString();
+        // 現在のスキルポイントとプレイヤーの持っているポイントが異なれば
+        if (this.now_skillpoint != playerData.skillPoint)
+        {
+            // ポイントを入れなおす
+            this.now_skillpoint = playerData.skillPoint;
+        }
+    }
+
+}
--- mmo/Assets/Script/Game/Character/Warrior.cs
+++ mmo/Assets/Script/Game/Character/Warrior.cs
@@ -3,31 +3,83 @@ using System.Collections;
 
 public class Warrior : PlayerChar {
 
-    private bool isSkills = false;
+    bool isUseSkills = false;
+    bool isStan = false;
 
     public override bool UseSkill(int skillNumber, SkillBase skill)
     {
-        // スキルを使用中に切り替える
-        SkillFlag();
-
-        if (skill.GetSp() > SP || isSkills)
+        // デバッグ用
+        Debug.Log("Call UseSkil");
+        
+        if (skill.GetSp() > SP || isUseSkills)
         {
+            Debug.Log("Skill Cancel");
             // スキル使用させない
             return false;
         }
 
+        Debug.Log("Spend SP");
         // SPを消費させる
         SP -= skill.GetSp();
+
+        // スキルを使用中に切り替える
+        SkillFlag();
+
         // スキルのIDで処理分けを行う
-        
         switch (skillNumber)
         {
             case 0:
-                this.Berserk();
-                break;
+                    // 挑発
+                    break;
+            case 1:
+                    this.Berserk();
+                    break;
+            case 2:
+                    this.Warcry();
+                    break;
+            case 3:
+                    this.BloodRage();
+                    break;
+            case 4:
+                    this.Defender();
+                    break;
+            case 5:
+                    this.Lampart();
+                    break;
+            case 6:
+                    this.Sentinel();
+                    break;
+            case 7:
+                    this.PowerSlash();
+                    break;
+            case 8:
+                    this.Fastblade();
+                    break;
+            case 9:
+                    this.DashSword();
+                    break;
+            case 10:
+                    this.SonicBoom();
+                    break;
+            case 11:
+                    this.SpiritsWithIn();
+                    break;
+            case 12:
+                    this.DrainSword();
+                    break;
+            case 13:
+                    this.SpinSlash();
+                    break;
+            case 14:
+                    this.BoparuSword();
+                    break;
+            case 15:
+                    this.ShieldBash();
+                    break;
+
         }
+
         return true;
-        
     }
 
     /// <summary>
@@ -37,12 +89,16 @@ public class Warrior : PlayerChar {
     {
         SkillBase berserk = SkillControl.GetSkill("バーサク");
 
+        Debug.Log("バーサク");
         // 再生させるアニメーションを指定する
-        anim.SetTrigger("Buff");
+        anim.SetTrigger("Buffs");
         // 攻撃力アップ
         StartCoroutine(StrBuff(berserk.GetAttack(), berserk.GetEffectTime()));
         // 防御力ダウン
         StartCoroutine(DefBuff(berserk.GetDefence(), berserk.GetEffectTime()));
+        Debug.Log(playerData.attack);
+        // スキルフラグをfalseにしておく
+        this.EndSkillFlag();
     }
 
     /// <summary>
@@ -53,9 +109,11 @@ public class Warrior : PlayerChar {
         SkillBase warcry = SkillControl.GetSkill("ウォークライ");
 
         // 再生させるアニメーションを指定する
-        anim.SetTrigger("Buff");
+        anim.SetTrigger("Buffs");
         // 攻撃力アップ
         StartCoroutine(StrBuff(warcry.GetAttack(), warcry.GetEffectTime()));
+        // スキルフラグをfalseにしておく
+        this.EndSkillFlag();
     }
 
     /// <summary>
@@ -66,11 +124,13 @@ public class Warrior : PlayerChar {
         SkillBase bloodrage = SkillControl.GetSkill("ブラッドレイジ");
 
         // 再生させるアニメーションを指定する
-        anim.SetTrigger("Buff");
+        anim.SetTrigger("BloodRage");
         // 攻撃力アップ
         StartCoroutine(StrBuff(bloodrage.GetAttack(), bloodrage.GetEffectTime()));
         // 防御力ダウン
-        StartCoroutine(DefBuff(bloodrage.GetDefence(), bloodrage.GetEffectTime())); 
+        StartCoroutine(DefBuff(bloodrage.GetDefence(), bloodrage.GetEffectTime()));
+        // スキルフラグをfalseにしておく
+        this.EndSkillFlag();
     }
 
     /// <summary>
@@ -81,9 +141,11 @@ public class Warrior : PlayerChar {
         SkillBase defender = SkillControl.GetSkill("ディフェンダー");
 
         // 再生させるアニメーションを指定する
-        anim.SetTrigger("Buff");
+        anim.SetTrigger("Buffs");
         // 物理防御力アップ
         StartCoroutine(DefBuff(defender.GetAttack(), defender.GetEffectTime()));
+        // スキルフラグをfalseにしておく
+        this.EndSkillFlag();
     }
 
     /// <summary>
@@ -94,9 +156,11 @@ public class Warrior : PlayerChar {
         SkillBase lampart = SkillControl.GetSkill("ランパート");
 
         // 再生させるアニメーションを指定する
-        anim.SetTrigger("Buff");
+        anim.SetTrigger("Buffs");
         // 物理防御力アップ
         StartCoroutine(DefBuff(lampart.GetAttack(), lampart.GetEffectTime()));
+        // スキルフラグをfalseにしておく
+        this.EndSkillFlag();
     }
 
     /// <summary>
@@ -112,6 +176,8 @@ public class Warrior : PlayerChar {
         StartCoroutine(DefBuff(sentinel.GetAttack(), sentinel.GetEffectTime()));
         // 魔法防御力アップ
         StartCoroutine(MndBuff(sentinel.GetAttack(), sentinel.GetEffectTime()));
+        // スキルフラグをfalseにしておく
+        this.EndSkillFlag();
     }
 
     /// <summary>
@@ -133,28 +199,185 @@ public class Warrior : PlayerChar {
     }
 
     /// <summary>
+    /// パワースラッシュ(攻撃系)
+    /// </summary>
+    public void PowerSlash()
+    {
+        SkillBase powerslash = SkillControl.GetSkill("パワースラッシュ");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * powerslash.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("PowerSlash");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
+    /// ダッシュソード(攻撃系)
+    /// </summary>
+    public void DashSword()
+    {
+        SkillBase dashsword = SkillControl.GetSkill("ダッシュ斬り");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * dashsword.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("DashSword");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
+    /// ソニックブーム(攻撃系) ShockWave
+    /// </summary>
+    public void SonicBoom()
+    {
+        SkillBase sonicboom = SkillControl.GetSkill("ソニックブーム");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * sonicboom.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("ShockWave");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
+    /// スピリッツウィズイン(攻撃系)
+    /// </summary>
+    public void SpiritsWithIn()
+    {
+        SkillBase spiritswithin = SkillControl.GetSkill("スピリッツウィズイン");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * spiritswithin.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("SpiritsWithIn");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
+    /// ドレインソード(攻撃系)
+    /// </summary>
+    public void DrainSword()
+    {
+        SkillBase drainsword = SkillControl.GetSkill("ドレインソード");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * drainsword.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("DrainSword");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
+    /// スピンスラッシュ(攻撃系) 元RotationSword
+    /// </summary>
+    public void SpinSlash()
+    {
+        SkillBase spinslash = SkillControl.GetSkill("スピンスラッシュ");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * spinslash.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("RotationSword");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
+    /// ボーパルソード(攻撃系)
+    /// </summary>
+    public void BoparuSword()
+    {
+        SkillBase boparusword = SkillControl.GetSkill("ボーパルソード");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * boparusword.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("BoparuSword");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
+    /// シールドバッシュ(攻撃系)
+    /// </summary>
+    public void ShieldBash()
+    {
+        SkillBase shiledbash = SkillControl.GetSkill("シールドバッシュ");
+
+        // ダメージ計算式
+        int damage = playerData.attack + (int)(playerData.attack * shiledbash.GetAttack());
+
+        // スキルごとのダメージを設定する
+        SetAttack(damage);
+        // スタンフラグをtrueに変える
+        this.SetStanFlag();
+        // 再生させるアニメーションを指定する
+        anim.SetTrigger("ShiledBash");
+        // アタック中に切り替える
+        Attack();
+    }
+
+    /// <summary>
     /// スキルを使用した時
     /// </summary>
-    public void SkillFlag()
-    {
-        isSkills = true;
-    }
+    public void SkillFlag() { this.isUseSkills = true; }
 
     /// <summary>
     /// スキルを使い終わった時
     /// </summary>
-    public void EndSkillFlag()
-    {
-        isSkills = false;
-    }
+    public void EndSkillFlag() { this.isUseSkills = false; }
 
+    /// <summary>
+    /// スタンフラグの取得
+    /// </summary>
+    /// <returns></returns>
+    public bool GetStanFlag() { return this.isStan; }
+    
+    /// <summary>
+    /// スタンフラグをtrueにする
+    /// </summary>
+    protected void SetStanFlag() { this.isStan = true; }
+    
+    /// <summary>
+    /// スタンフラグをfalseにする
+    /// </summary>
+    protected void EndStanFlag() { this.isStan = false; }
+    
     /// <summary>
     /// アタックに切り替え
     /// </summary>
     protected override void Attack()
     {
+        // Baseクラスに書いてある処理をさせる
         base.Attack();
-    } 
+    }
 
     protected override void Damage()
     {
@@ -173,10 +396,23 @@ public class Warrior : PlayerChar {
 
     }
 
+    /// <summary>
+    /// 詠唱が終了したモーションが終了したら呼び出される
+    /// </summary>
+    public override void EndAttackAnimation()
+    {
+        // 基礎クラスの処理を行う
+        base.EndAttackAnimation();
+    }
+
     protected override void EndOfAttack()
     {
-        base.DisablePlayerAttack();
-        EndSkillFlag();
+        // Baseクラスに書いてある処理をさせる
+        base.EndOfAttack();
+        // スキルフラグをfalseにしておく
+        this.EndSkillFlag();
+        // スタンフラグをfalseにしておく
+        this.EndStanFlag();
     }
 
     // 今のところ使用しない
@@ -200,4 +436,5 @@ public class Warrior : PlayerChar {
     {
         base.Start();
     }
+
 }
